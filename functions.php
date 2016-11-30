883f75605e5b0c63f9d8c09e52d7bcefdedde95d
geitplak <geitplak@greeny.cs.tlu.ee>
2016-11-30 08:42:20 +0200

    sõnum muudatustest

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+	require("/home/geithy/config.php");
+
+	/* ALUSTAN SESSIOONI */
+	session_start();
+		
+	/* ÜHENDUS */
+	$database = "if16_geithy";
+	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+	
+
+?>
